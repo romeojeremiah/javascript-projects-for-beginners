1c14ef6c9165cd4f89ae1113af7590c90fe0edb0
gaizkamg <57919401+gaizkamg@users.noreply.github.com>
2020-11-13 19:48:34 +0100

    Update script.js
    The length of the array wil be -1 the order of the array. Array order 0-5, array lenght 6. So Math.random()*colors.length+1 will drop some undefined positions.

--- Change_Color_Background/js/script.js
+++ Change_Color_Background/js/script.js
@@ -7,6 +7,6 @@ body.style.backgroundColor = 'violet'
 button.addEventListener('click', changeBackground)
 
 function changeBackground(){
-const colorIndex= parseInt(Math.random()*colors.length+1)
+const colorIndex= parseInt(Math.random()*colors.length-1)
 body.style.backgroundColor = colors[colorIndex]
 }
